ef0b3ece53f3fdcf82694aa07de80c75dbe9b47e
Juangmcz <juangui-089@hotmail.com>
2023-04-24 10:11:22 -0500

    Angular Material was added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
