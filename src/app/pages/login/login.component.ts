fd502e82790ca7dc0feed669c097a188f09f6bda
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-25 14:58:31 -0500

    Color changes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SwalComponent} from "@sweetalert2/ngx-sweetalert2";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit{
+
+  title: string = 'Logged in';
+  iconn: string = 'success';
 
   form: FormGroup = new FormGroup({})
   constructor(private builder: FormBuilder) { }
@@ -16,5 +20,20 @@ export class LoginComponent implements OnInit{
       lastName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]]
     })
+  }
+
+  @ViewChild('confirmRejectSwal')
+  public readonly confirmRejectSwal!: SwalComponent;
+
+  onSubmit(){
+    if(this.form.valid){
+      this.title = 'Logged in';
+      this.iconn = 'success';
+      this.confirmRejectSwal.fire()
+    }else{
+      this.iconn = 'error';
+      this.title = 'Not logged in';
+      this.confirmRejectSwal.fire()
+    }
   }
 }
