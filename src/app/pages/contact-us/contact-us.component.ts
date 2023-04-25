436cc51605837a84003e09c153f0d7076815780c
Juangmcz <juangui-089@hotmail.com>
2023-04-25 14:59:20 -0500

    Implemented contact us page

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent {
+  constructor() {}
 
+  onClick() {
+    //TODO: Add logic to send email
+  }
 }
