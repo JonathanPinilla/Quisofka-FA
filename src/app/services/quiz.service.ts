984c23654bf3ecfb845c8529389d4e54096df6f8
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-26 14:31:23 -0500

    Quiz service generate tests of the right level

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Quiz} from "../models/quiz";
@@ -9,10 +9,26 @@ import {Quiz} from "../models/quiz";
 export class QuizService {
 
   private api: string = 'https://quisofka-mvn-b-production.up.railway.app/quisofka/quizzes/quizzes';
-  constructor(private http: HttpClient) { }
 
-  generateQuiz(studentId: string){
-    return this.http.post(this.api, {studentId: studentId});
+  constructor(private http: HttpClient) {
+  }
+
+  generateQuiz(studentId: string, level: string) {
+    var levelPath = "";
+    switch (level.toLowerCase()) {
+      case "initial":
+        levelPath = "firstlvl";
+        break;
+
+      case "basic":
+        levelPath = "secondlvl";
+        break;
+
+      case "intermediate":
+        levelPath = "thirdlvl";
+        break;
+    }
+    return this.http.post(this.api + "/" + levelPath, {studentId: studentId});
   }
 
 }
