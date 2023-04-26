a514d4c8d64945576a88e71979961f654b652367
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-26 10:53:37 -0500

    Question model added

---
+++ src/app/models/question.ts
@@ -0,0 +1,15 @@
+export interface Question {
+
+  "id": string,
+  "description": string,
+  "answers": {
+    "additionalProp1": boolean,
+    "additionalProp2": boolean,
+    "additionalProp3": boolean
+  },
+  "knowledgeArea": string,
+  "descriptor": string,
+  "type": string,
+  "level": string
+
+}
